0e6cfb09d8dc78b902da54948142a84647863a43
Itsskiip <70840095+Itsskiip@users.noreply.github.com>
2024-10-18 20:21:52 +0800

    initial commit

---
+++ conversion.py
@@ -0,0 +1,20 @@
+with open("asdf.txt") as f:
+    text = f.read()
+
+arr = text.split("\\right),\\left(")
+arr[0] = arr[0].split('(')[-1]
+arr[-1] = arr[-1].split('\\')[0]
+
+total_x = 0
+total_y = 0
+
+for a in arr:
+    split = a.split(",")
+    total_x += float(split[0])
+    total_y += float(split[1])
+
+print(total_x / len(arr))
+print(total_y / len(arr))
+
+# with open("output.txt", "w") as f:
+#     f.write(",".join(map(lambda s: "(" + s + ")", arr)))
